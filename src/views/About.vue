28be4adb023751d31af88cf46d7d447d970801e0
muzi131313 <muzi131313@163.com>
2019-11-26 12:03:48 +0800

    axios-pro: add usage in vue file

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,51 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul class="about-list">
+      <li v-for="d in data" :key="d.code">{{d.name}}</li>
+    </ul>
   </div>
 </template>
+<script>
+/**
+ * @file views/About.vue
+ * @description 关于页面
+ * @created 2019年11月26日10:39:33
+ */
+export default {
+  name: 'About',
+  data() {
+    return {
+      data: []
+    }
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    async init() {
+      const resp = await this.$api.queryOrg({
+        jsonConditions: {
+          op: 'or',
+          elements: [
+            {
+              param: 'name',
+              op: 'contains',
+              values: '北京'
+            }
+          ]
+        }
+      })
+      const content = resp.content
+      this.data = content
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.about {
+  &-list {
+    list-style: none;
+  }
+}
+</style>
